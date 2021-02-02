ae7a8ac036ce3bf6d7af82b8e0afadf5fc92326d
mascanho <mascanho@sapo.pt>
2021-02-02 15:10:02 +0000

    updates
    logo on the bubble

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,3 +1,12 @@
-$('.rad2').on('mouseenter mouseleave', function () {
-  $('.box2, .line2').toggleClass('open');
+$('.rad2').on('mouseenter mouseleave', function() {
+    $('.box2, .line2').toggleClass('open');
 });
+
+
+// Pop  up warning 
+
+document.getElementById("pt-active").addEventListener("click", changeLanguage);
+
+function changeLanguage() {
+    alert('You will now be redirected to the Portuguese version of this website')
+}
