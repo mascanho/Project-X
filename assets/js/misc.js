342f0833c9c517c27fc30e87342915a86951e41e
mascanho <mascanho@sapo.pt>
2021-02-02 16:13:24 +0000

    Updates
    Almost final updates

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -8,5 +8,14 @@ $('.rad2').on('mouseenter mouseleave', function() {
 document.getElementById("pt-active").addEventListener("click", changeLanguage);
 
 function changeLanguage() {
-    alert('You will now be redirected to the Portuguese version of this website')
+    // alert('You will now be redirected to the Portuguese version of this website')
+    // setTimeout("window.location.href = 'http://walkerwines.com.au/' ", 10000);
+    alert('You will be going to the Portuguese of This website')
+    window.location.href = "#";
+
 }
+
+
+// Sending use to another link after a timeout
+
+// setTimeout("window.location.href = 'http://walkerwines.com.au/' ", 5 * 1000);
