15ab006ab2d7fe3848df3d94d61f67953900d9e2
mascanho <mascanho@sapo.pt>
2021-02-04 20:56:34 +0000

    changes
    changes

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,19 +1,16 @@
-$('.rad2').on('mouseenter mouseleave', function() {
-    $('.box2, .line2').toggleClass('open');
+$('.rad2').on('mouseenter mouseleave', function () {
+  $('.box2, .line2').toggleClass('open');
 });
 
+// Pop  up warning when changing language to portuguese
 
-// Pop  up warning 
-
-document.getElementById("pt-active").addEventListener("click", changeLanguage);
+document.getElementById('pt-active').addEventListener('click', changeLanguage);
 
 function changeLanguage() {
-    // alert('You will now be redirected to the Portuguese version of this website')
-    // setTimeout("window.location.href = 'http://walkerwines.com.au/' ", 10000);
-    alert('You have selected:  🇵🇹 Portuguese. ')
-    setTimeout(window.location.href = "#", 1 * 10000);
-
-
+  // alert('You will now be redirected to the Portuguese version of this website')
+  // setTimeout("window.location.href = 'http://walkerwines.com.au/' ", 10000);
+  alert('You have selected:  🇵🇹 Portuguese. ');
+  setTimeout((window.location.href = '#'), 1 * 10000);
 }
 
 // Function when the page loads
@@ -23,8 +20,6 @@ function changeLanguage() {
 //     doSomethingElse();
 // };
 
-
-
 // Sending use to another link after a timeout
 
 // setTimeout("window.location.href = 'http://walkerwines.com.au/' ", 5 * 1000);
@@ -32,16 +27,16 @@ function changeLanguage() {
 // When it click the CSS class .english
 const modalBtn = document.querySelector('.english');
 
-
 const modalBg = document.querySelector('.modal-bg');
 
-const modalClose = document.querySelector('.modal-close')
+const modalClose = document.querySelector('.modal-close');
 
-modalBtn.addEventListener('click', function() {
-    modalBg.classList.add('bg-active');
+modalBtn.addEventListener('click', function () {
+  modalBg.classList.add('bg-active');
 });
 
-modalClose.addEventListener('click', function() {
-    modalBg.classList.remove('bg-active');
-
+modalClose.addEventListener('click', function () {
+  modalBg.classList.remove('bg-active');
 });
+
+// TESTING
