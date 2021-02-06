bb3cea7e165789844cc3c99106e4ef68a146381d
mascanho <mascanho@sapo.pt>
2021-02-06 19:06:39 +0000

    galeria com update
    Galeria implementada

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,48 +1,7 @@
-const imgContent = document.querySelectorAll('.img-content-hover');
-
-function showImgContent(e) {
-  for (var i = 0; i < imgContent.length; i++) {
-    x = e.pageX;
-    y = e.pageY;
-    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
-  }
+var quadimages = document.querySelectorAll('#quad figure');
+for (i = 0; i < quadimages.length; i++) {
+  quadimages[i].addEventListener('click', function () {
+    this.classList.toggle('expanded');
+    quad.classList.toggle('full');
+  });
 }
-
-// portfolio
-var gallery = document.querySelector('#gallery');
-var getVal = function (elem, style) {
-  return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
-};
-var getHeight = function (item) {
-  return item.querySelector('.content').getBoundingClientRect().height;
-};
-var resizeAll = function () {
-  var altura = getVal(gallery, 'grid-auto-rows');
-  var gap = getVal(gallery, 'grid-row-gap');
-  gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-    var el = item;
-    el.style.gridRowEnd =
-      'span ' + Math.ceil((getHeight(item) + gap) / (altura + gap));
-  });
-};
-gallery.querySelectorAll('img').forEach(function (item) {
-  item.classList.add('byebye');
-  if (item.complete) {
-    console.log(item.src);
-  } else {
-    item.addEventListener('load', function () {
-      var altura = getVal(gallery, 'grid-auto-rows');
-      var gap = getVal(gallery, 'grid-row-gap');
-      var gitem = item.parentElement.parentElement;
-      gitem.style.gridRowEnd =
-        'span ' + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
-      item.classList.remove('byebye');
-    });
-  }
-});
-window.addEventListener('resize', resizeAll);
-gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-  item.addEventListener('click', function () {
-    item.classList.toggle('full');
-  });
-});
