d65225e7cc5ccbeef5553bfd06c6bb176c7567dd
mascanho <mascanho@sapo.pt>
2021-03-01 20:11:16 +0000

    updates
    updates

---
+++ assets/js/cookies.js
@@ -0,0 +1,13 @@
+// const header = document.querySelector('section');
+
+// const message = document.createElement('div');
+// message.classList.add('cookies');
+// message.innerHTML =
+//   'We use cookie on this website <button class="btn btn-close">OK</button>';
+// header.append(message);
+
+// const closeMessage = message
+//   .querySelector('.btn-close')
+//   .addEventListener('click', function () {
+//     header.remove();
+//   });
