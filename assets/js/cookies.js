a321ff87ae2e3ce34f74b1620eca2d168a0df662
mascanho <mascanho@sapo.pt>
2021-03-09 20:35:38 +0000

    updates
    updates

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -1,13 +1,36 @@
-// const header = document.querySelector('section');
+// COOKIES
 
-// const message = document.createElement('div');
-// message.classList.add('cookies');
-// message.innerHTML =
-//   'We use cookie on this website <button class="btn btn-close">OK</button>';
-// header.append(message);
+function PopUp(hideOrshow) {
+  if (hideOrshow === 'hide') {
+    document.getElementById('ac-wrapper').style.display = 'none';
+  } else if (
+    localStorage.getItem('popupWasShown') !== '1' &&
+    hideOrshow === 'show'
+  ) {
+    document.getElementById('ac-wrapper').removeAttribute('style');
+    localStorage.setItem('popupWasShown', '1');
+  }
+}
 
-// const closeMessage = message
-//   .querySelector('.btn-close')
-//   .addEventListener('click', function () {
-//     header.remove();
-//   });
+// window.onload = function () {
+//   setTimeout(function () {
+//     PopUp('show');
+//   }, 1000);
+// };
+
+window.addEventListener('load', function () {
+  setTimeout(function () {
+    PopUp('show');
+  }, 1000);
+});
+
+function hideNow(e) {
+  if (e.target.id == 'ac-wrapper') {
+    document.getElementById('ac-wrapper').style.display = 'none';
+    localStorage.setItem('popupWasShown', '1');
+  }
+}
+
+document.getElementById('reset').onclick = function () {
+  localStorage.setItem('popupWasShown', '3');
+};
