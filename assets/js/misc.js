4c1150c2b4536632724b31ff72082e51aa3bf238
mascanho <mascanho@sapo.pt>
2021-03-19 17:47:27 +0000

    updates
    updates

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,18 +1,3 @@
 $('.rad2').on('mouseenter mouseleave', function () {
   $('.box2, .line2').toggleClass('open');
 });
-
-// Pop  up warning when changing language to portuguese
-
-// document.getElementById('pt-active').addEventListener('click', changeLanguage);
-
-// function changeLanguage() {
-//   // alert('You will now be redirected to the Portuguese version of this website')
-//   // setTimeout("window.location.href = 'http://walkerwines.com.au/' ", 10000);
-//   alert('You have selected:  🇵🇹 Portuguese. ');
-//   setTimeout((window.location.href = '#'), 1 * 10000);
-// }
-
-// Sending use to another link after a timeout
-
-// setTimeout("window.location.href = 'http://walkerwines.com.au/' ", 5 * 1000);
